d902bf6da54c93c680ce33c7e415cdb3935ff0d3
Ludwig-Uni <ludwig.kolesch@student.uni-tuebingen.de>
2023-06-01 23:39:43 +0200

    Add --help command line argument and welcome message

--- LazyMathInstructor/Program.cs
+++ LazyMathInstructor/Program.cs
@@ -389,11 +389,34 @@
         }
 
         /// <summary>
+        /// Print help message when the program is called with -h or --help command line argument.
+        /// </summary>
+        static void PrintHelpMessage()
+        {
+            Console.WriteLine("LazyMathInstructor by Ludwig Kolesch\n" +
+                "Source and explanation: https://github.com/Ludwig-Uni/Lazy-Math-Instructor\n" +
+                "Usage: Enter the number of equivalences to test and all the terms via STDIN.\n" +
+                "Terminate each input with a newline.\n" +
+                "\n" +
+                "Options:\n" +
+                " -v, --verbose: Output parsed terms\n" +
+                " -h, --help:    Display this message");
+        }
+
+        /// <summary>
         /// Entry point of the program. Reads input from STDIN and prints YES or NO for each pair of terms
         /// entered, depending on whether they are equivalent, according to the specification.
         /// </summary>
         static void Main(string[] args)
         {
+            if (args.Contains("-h") || args.Contains("--help"))
+            {
+                PrintHelpMessage();
+                return;
+            }
+
+            Console.WriteLine("LazyMathInstructor by Ludwig Kolesch\nReading input from STDIN now.\n");
+
             bool verbose = args.Contains("-v") || args.Contains("--verbose");
 
             var terms = GetInputTerms();
